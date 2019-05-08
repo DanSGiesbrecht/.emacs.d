0d926422d1e456c71981cccf426c009c00cef5f5
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-05-08 17:01:22 -0600

    Initial Commit

---
+++ init.el
@@ -0,0 +1,51 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile (require 'use-package))
+
+
+;; modes
+;; vim keymaps/functionality
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+;; powerline
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-center-evil-theme))
+(add-hook 'after-init-hook 'powerline-reset)
+
+;; bind modes to filetypes
+(add-to-list 'auto-mode-alist '("\\.dcm$" . text-mode))
+;;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; themes
+(use-package monokai-alt-theme
+  :ensure t
+  :load-path "themes"
+  :config
+  (load-theme 'monokai-alt t))
+
+;; org mode mods
+(setq org-hide-emphasis-markers t)
+
+(font-lock-add-keywords 'org-mode
+			'(("^ *\\([-]\\) "
+			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(use-package org-bullets
+  :ensure t
+  :init
+  (setq org-bullets-bullet-list)
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
